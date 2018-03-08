5f4d58f5b803f6efc3674c2fd765090860a6135c
Muhammad Furqan Habibi <furqan.habibi@isid.co.jp>
2018-03-08 22:32:26 +0900

    finish mapping and navigation

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
 # keigan_motor
+
+### Dependencies
+```
+$ cd ~/catkin_ws/src
+$ git clone https://github.com/intel-ros/realsense
+$ mv realsense/realsense2_camera .
+$ cd ~/catkin_ws
+$ rosdep install --from-paths src --ignore-src -r -y
+```
+
+### Setup
+```
+$ cd ~/catkin_ws
+$ catkin_make
+$ source devel/setup.bash
+$ sh $(rospack find keigan_motor_bringup)/scripts/create_udev_rules.sh
+```
--- keigan_motor_bringup/scripts/create_udev_rules.sh
+++ keigan_motor_bringup/scripts/create_udev_rules.sh
@@ -37,8 +37,8 @@
 echo "remap the device serial port(ttyUSBX) to  rplidar"
 echo "rplidar usb connection as /dev/rplidar , check it using the command : ls -l /dev|grep ttyUSB"
 echo "start copy rplidar.rules to  /etc/udev/rules.d/"
-echo "`rospack find roomblock_bringup`/scripts/rplidar.rules"
-sudo cp `rospack find roomblock_bringup`/scripts/rplidar.rules  /etc/udev/rules.d
+echo "`rospack find keigan_motor_bringup`/scripts/rplidar.rules"
+sudo cp `rospack find keigan_motor_bringup`/scripts/rplidar.rules  /etc/udev/rules.d
 
 echo " "
 echo "Restarting udev"
